db1ad9cbdad95ad841f270703ca5ced69c9f3c15
Tahir-yameen <tricky_hackers327@hotmail.com>
2024-04-20 02:55:14 +0500

    updated features

--- main.js
+++ main.js
@@ -4,24 +4,82 @@ import chalk from "chalk";
 let toDoList = [];
 let conditions = true;
 console.log(chalk.yellow.bold("\n \t ~^~^~^~^~^ Welcome to my Cli based ToDo_List ^~^~^~^~^~ \n"));
-while (conditions) {
+let funmain = async () => {
+    while (conditions) {
+        let category = await inquirer.prompt([
+            {
+                name: "options",
+                type: "list",
+                message: (chalk.magenta.bold("Please Select Your Option:- ")),
+                choices: ["Add Task", "Update Task", "Delete Task", "View ToDo-List", "Exit"],
+            }
+        ]);
+        if (category.options === "Add Task") {
+            await addTask();
+        }
+        else if (category.options === "Delete Task") {
+            await delTask();
+        }
+        else if (category.options === "Update Task") {
+            await updateTask();
+        }
+        else if (category.options === "View ToDo-List") {
+            await viewTask();
+        }
+        else if (category.options === "Exit") {
+            conditions = false;
+        }
+    }
+};
+// Function for adding new task:
+let addTask = async () => {
     let addTask = await inquirer.prompt([
         {
             name: "task",
             type: "input",
-            message: (chalk.magenta.bold("Please Enter Your New Task:- "))
+            message: (chalk.magenta.bold("Please Enter Your New Task:-"))
         }
     ]);
     toDoList.push(addTask.task);
-    console.log(chalk.red.bold(addTask.task), "Task Added To Your ToDo-List Successfully:- ");
-    let addMoreTask = await inquirer.prompt([
+    console.log(chalk.red.bold(`\n${addTask.task}`), `Task Added To Your ToDo-List Successfully:- \n`);
+};
+// Function for viewing ToDo-List task:
+let viewTask = () => {
+    console.log(`\n Your ToDo-List:- \n`);
+    toDoList.forEach((task, index) => {
+        console.log(`${index + 1}: ${task}`);
+    });
+    console.log("\n");
+};
+// Function for deleting tasks:
+let delTask = async () => {
+    await viewTask();
+    let taskDelete = await inquirer.prompt([
         {
-            name: "addmore",
-            type: "confirm",
-            message: (chalk.magenta.bold("Do You Want To Add More Task:- ")),
-            default: "false"
+            name: "sno",
+            type: "number",
+            message: chalk.magenta.bold("Please Enter The 'Serial No.' Of The Task To Delete:-"),
         }
     ]);
-    conditions = addMoreTask.addmore;
-}
-console.log(chalk.green.bold(chalk.red("Your Updated Todo-list:- ")), toDoList);
+    let taskDeleted = toDoList.splice(taskDelete.sno - 1, 1);
+    console.log(`\n ${taskDeleted} This Task Has Been Deleted Succesfully From Your ToDo-List \n`);
+};
+//Function for Updating Task:
+let updateTask = async () => {
+    await viewTask();
+    let taskUpdate = await inquirer.prompt([
+        {
+            name: "utask",
+            type: "number",
+            message: chalk.magenta.bold("Please Enter The 'Serial No.' Of The Task You Want To Update:-"),
+        },
+        {
+            name: "newutask",
+            type: "input",
+            message: chalk.magenta.bold("Please Enter The New Task:-"),
+        }
+    ]);
+    toDoList[taskUpdate.utask - 1] = taskUpdate.newutask;
+    console.log(`\n Task at the S.no ${taskUpdate.utask} Updated Successfully [For Updated List Check Option: 'View ToDo-List']\n`);
+};
+funmain();
--- main.ts
+++ main.ts
@@ -3,30 +3,92 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
 
-let toDoList = [];
+let toDoList: string [] = [];
 let conditions = true;
 
 console.log(chalk.yellow.bold("\n \t ~^~^~^~^~^ Welcome to my Cli based ToDo_List ^~^~^~^~^~ \n"));
 
-while (conditions) {
+let funmain = async () => {
+  while(conditions){
+    let category = await inquirer.prompt([
+      {
+        name: "options",
+        type: "list",
+        message: (chalk.magenta.bold("Please Select Your Option:- ")),
+        choices: ["Add Task", "Update Task", "Delete Task", "View ToDo-List", "Exit"],
+      }
+    ]);
+    if(category.options === "Add Task"){
+      await addTask()
+    }
+    else if(category.options === "Delete Task"){
+      await delTask()
+    }
+    else if(category.options === "Update Task"){
+      await updateTask()
+    }
+    else if(category.options === "View ToDo-List"){
+      await viewTask()
+    }
+    else if(category.options === "Exit"){
+      conditions = false
+    }
+  }
+}
+
+// Function for adding new task:
+let addTask = async () => {
   let addTask = await inquirer.prompt([
     {
       name: "task",
       type: "input",
-      message: (chalk.magenta.bold("Please Enter Your New Task:- "))
+      message: (chalk.magenta.bold("Please Enter Your New Task:-"))
     }
   ]);
   toDoList.push(addTask.task);
-  console.log(chalk.red.bold(addTask.task) , "Task Added To Your ToDo-List Successfully:- ");
+  console.log(chalk.red.bold(`\n${addTask.task}`), `Task Added To Your ToDo-List Successfully:- \n`);
+}
 
-  let addMoreTask = await inquirer.prompt([
+// Function for viewing ToDo-List task:
+let viewTask = () => {
+  console.log(`\n Your ToDo-List:- \n`);
+  toDoList.forEach((task, index) => {
+    console.log(`${index + 1}: ${task}`)
+  });
+  console.log("\n");
+}
+
+// Function for deleting tasks:
+let delTask = async () => {
+  await viewTask()
+  let taskDelete = await inquirer.prompt([
     {
-      name: "addmore",
-      type: "confirm",
-      message: (chalk.magenta.bold("Do You Want To Add More Task:- ")),
-      default: "false"
+      name: "sno",
+      type: "number",
+      message: chalk.magenta.bold("Please Enter The 'Serial No.' Of The Task To Delete:-"),
     }
   ]);
-  conditions = addMoreTask.addmore;
+  let taskDeleted = toDoList.splice(taskDelete.sno - 1, 1);
+  console.log(`\n ${taskDeleted} This Task Has Been Deleted Succesfully From Your ToDo-List \n`);
 }
-console.log(chalk.green.bold(chalk.red("Your Updated Todo-list:- ")), toDoList);
+
+//Function for Updating Task:
+let updateTask = async () => {
+  await viewTask()
+  let taskUpdate = await inquirer.prompt([
+    {
+      name: "utask",
+      type: "number",
+      message: chalk.magenta.bold("Please Enter The 'Serial No.' Of The Task You Want To Update:-"),
+    },
+    {
+      name: "newutask",
+      type: "input",
+      message: chalk.magenta.bold("Please Enter The New Task:-"),
+    }
+  ]);
+  toDoList[taskUpdate.utask - 1] = taskUpdate.newutask;
+  console.log(`\n Task at the S.no ${taskUpdate.utask} Updated Successfully [For Updated List Check Option: 'View ToDo-List']\n`)
+}
+
+funmain();
